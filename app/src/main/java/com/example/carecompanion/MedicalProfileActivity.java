3a89e43beb733f50e817f75a22ecbe0c0ac7ca6a
a-aima0 <aima10fatima@gmail.com>
2024-07-19 15:25:18 +0100

    medicalDataRetrieval
    - display all the information on the medicalprofileactivity, retrieving from firebase
    - update the medicalprofilelayout UI to include new fields
    - included additional info field

--- app/src/main/java/com/example/carecompanion/MedicalProfileActivity.java
+++ app/src/main/java/com/example/carecompanion/MedicalProfileActivity.java
@@ -28,6 +28,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,7 +39,8 @@ public class MedicalProfileActivity extends AppCompatActivity {
 
     public static final String TAG = "MedicalProfileActivity";
     Button gotoCreateMedicalProfileButton, gotoProfileButon;
-    TextView medicalWeight, medicalHeight, medicalDOB, medicalAddress, medicalBlood, medicalOrgan;
+    TextView medicalWeight, medicalHeight, medicalDOB, medicalAddress, medicalBlood, medicalOrgan,
+            medicalAllergies, medicalConditions, medicalMedication, medicalContacts, medicalInfo;
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
     FirebaseUser user;
@@ -56,6 +58,12 @@ public class MedicalProfileActivity extends AppCompatActivity {
         medicalAddress = findViewById(R.id.medicalAddress);
         medicalBlood = findViewById(R.id.medicalBloodType);
         medicalOrgan = findViewById(R.id.medicalOrgan);
+        medicalInfo = findViewById(R.id.medicalInfo);
+
+        medicalAllergies = findViewById(R.id.medicalAllergies);
+        medicalConditions = findViewById(R.id.medicalConditions);
+        medicalMedication = findViewById(R.id.medicalMedications);
+        medicalContacts = findViewById(R.id.medicalContacts);
 
         gotoCreateMedicalProfileButton = findViewById(R.id.gotoCreateMedicalActivity);
         gotoProfileButon = findViewById(R.id.gotoProfile);
@@ -79,6 +87,95 @@ public class MedicalProfileActivity extends AppCompatActivity {
                         medicalAddress.setText(documentSnapshot.getString("address"));
                         medicalBlood.setText(documentSnapshot.getString("bloodType"));
                         medicalOrgan.setText(documentSnapshot.getString("organDonor"));
+                        medicalInfo.setText(documentSnapshot.getString("info"));
+
+                        documentReference.collection("allergies").get()
+                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        StringBuilder allergiesBuilder = new StringBuilder();
+                                        for (DocumentSnapshot allergyDoc : queryDocumentSnapshots){
+
+                                            allergiesBuilder.append("Allergy: ").append(allergyDoc.getString("name")).append("\n");
+                                            allergiesBuilder.append("Details: ").append(allergyDoc.getString("details")).append("\n");
+                                            allergiesBuilder.append("\n"); // Add a blank line to separate each allergy
+                                        }
+                                        medicalAllergies.setText(allergiesBuilder.toString().trim());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "onFailure: Error fetching allergies: " + e.getMessage());
+                                        Toast.makeText(MedicalProfileActivity.this, "Error fetching allergies", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                        documentReference.collection("contacts").get()
+                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        StringBuilder contactBuilder = new StringBuilder();
+                                        for (DocumentSnapshot contactDoc : queryDocumentSnapshots){
+
+                                            contactBuilder.append("Contact: ").append(contactDoc.getString("name")).append(" ")
+                                                    .append("(").append(contactDoc.getString("relation")).append(") ").append("\n");
+                                            contactBuilder.append("Phone: ").append(contactDoc.getString("phone")).append("\n");
+                                            contactBuilder.append("\n");
+                                        }
+                                        medicalContacts.setText(contactBuilder.toString().trim());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "onFailure: Error fetching allergies: " + e.getMessage());
+                                        Toast.makeText(MedicalProfileActivity.this, "Error fetching allergies", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                        documentReference.collection("conditions").get()
+                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        StringBuilder conditionsBuilder = new StringBuilder();
+                                        for (DocumentSnapshot conditionsDoc : queryDocumentSnapshots){
+
+                                            conditionsBuilder.append("Condition: ").append(conditionsDoc.getString("name")).append("\n");
+                                            conditionsBuilder.append("Details: ").append(conditionsDoc.getString("details")).append("\n");
+                                            conditionsBuilder.append("\n"); // Add a blank line to separate each allergy
+                                        }
+                                        medicalConditions.setText(conditionsBuilder.toString().trim());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "onFailure: Error fetching conditions: " + e.getMessage());
+                                        Toast.makeText(MedicalProfileActivity.this, "Error fetching conditions", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                        documentReference.collection("medications").get()
+                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        StringBuilder medicationsBuilder = new StringBuilder();
+                                        for (DocumentSnapshot medicationDoc : queryDocumentSnapshots){
+
+                                            medicationsBuilder.append("Medication: ").append(medicationDoc.getString("name")).append("\n");
+                                            medicationsBuilder.append("Details: ").append(medicationDoc.getString("details")).append("\n");
+                                            medicationsBuilder.append("\n"); // Add a blank line to separate each allergy
+                                        }
+                                        medicalMedication.setText(medicationsBuilder.toString().trim());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "onFailure: Error fetching medications: " + e.getMessage());
+                                        Toast.makeText(MedicalProfileActivity.this, "Error fetching medications", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+
+
                     } else {
                         Log.d(TAG, "No such document exists in profile collections");
                     }
